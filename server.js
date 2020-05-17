1de1856dee73990f752ac178218c3211d005140a
David Currie <david.alexander.currie@gmail.com>
2020-05-17 13:20:37 +1200

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const fetch = require("node-fetch");
+
+let url = "https://api.npoint.io/27499e1a2dc4c3170144";
+
+let settings = { method: "Get" };
+
+fetch(url, settings)
+  .then((res) => res.json())
+  .then((json) => {
+    // do something with JSON
+    app.get("/", (req, res) => {
+      res.status(200).json(json);
+    });
+  });
+
+app.listen(3000, () => console.log("server started"));
