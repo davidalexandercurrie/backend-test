5df4e6348f2bfd63cbfcfa054c5ba605dd0d81e1
David Currie <david.alexander.currie@gmail.com>
2020-06-01 16:19:47 +1200

    resetting

--- server.js
+++ server.js
@@ -14,19 +14,15 @@ server.listen(port);
 let url = "https://api.npoint.io/015816899430ca500cf1";
 
 let settings = { method: "Get" };
-let jsonData = {};
-setInterval(
-  fetch(url, settings)
-    .then((res) => res.json())
-    .then((json) => {
-      // do something with JSON
-      jsonData = json;
-    }),
-  1000 * 60 * 60
-);
-app.get("/", (req, res) => {
-  res.status(200).json(jsonData);
-});
+
+fetch(url, settings)
+  .then((res) => res.json())
+  .then((json) => {
+    // do something with JSON
+    app.get("/", (req, res) => {
+      res.status(200).json(json);
+    });
+  });
 
 // app.listen(port, () => console.log("server started on port!", port));
 
