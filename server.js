ea5d9064e2d8f46cbdd78782ce6d4fa8a3519d57
David Currie <david.alexander.currie@gmail.com>
2020-06-01 17:24:24 +1200

    faster for dev

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let url = "https://api.npoint.io/015816899430ca500cf1";
 
 let settings = { method: "Get" };
 
-setInterval(fetchData, 600000);
+setInterval(fetchData, 6000);
 fetch(url, settings)
   .then((res) => res.json())
   .then((json) => {
